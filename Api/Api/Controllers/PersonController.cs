c97f846f7f16b6f94bf00ef3905291dbb200a132
DicarProjects <Cardona.DevProjects@gmail.com>
2025-03-14 21:42:36 -0500

    Homework

--- Api/Api/Controllers/PersonController.cs
+++ Api/Api/Controllers/PersonController.cs
@@ -4,5 +4,46 @@ namespace Api.Controllers
 {
     public class PersonController : ControllerBase
     {
+
+        [HttpGet("DiegoTesting")]
+        public string DiegoTesting(string dameTuNombre)
+        {
+            var nombreObtenido = dameTuNombre;
+            //String nombreObtenido = dameTuNombre;
+
+            return "Hey There, your name is " + dameTuNombre;
+            //return $"Hey There, you name is {dameTuNombre}"; 
+        }
+
+
+        [HttpGet("Getallproducts")]
+        public string GetAllProducts()
+        {
+            //Modicafores de acceso 
+            //Tipos de retorno de un metodo,void,string
+            //Parametros
+            //Swagger que es ? en un api como se relaciona 
+
+            return "Todos los productos";
+        }
+
+        //2. Metodo GetProduct
+
+        [HttpGet("{id}")]
+        public string GetProduct(int id) 
+        {
+
+            // Modicafores de acceso 
+            // Clases
+            // Expresión Ternaria 
+            // Funciones de flecha vs funcion c# 
+            // que es un Objeto 
+            // Parametros
+
+            return "producto" + id;
+
+        } 
+
+        //3. Crear Una Lista de Productos 
     }
 }
