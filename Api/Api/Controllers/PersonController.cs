2a56838362b0b6da4204171e04a6a4c8061645a8
DicarProjects <Cardona.DevProjects@gmail.com>
2025-03-18 21:31:47 -0500

    Improve code

--- Api/Api/Controllers/PersonController.cs
+++ Api/Api/Controllers/PersonController.cs
@@ -5,40 +5,38 @@ namespace Api.Controllers
     public class PersonController : ControllerBase
     {
 
-        [HttpGet("DiegoTesting")]
-        public string DiegoTesting(string dameTuNombre)
-        {
-            var nombreObtenido = dameTuNombre;
-            //String nombreObtenido = dameTuNombre;
-
-            return "Hey There, your name is " + dameTuNombre;
-            //return $"Hey There, you name is {dameTuNombre}"; 
-        }
-
-
-        [HttpGet("Getallproducts")]
+        [HttpGet("GetAllProducts")]
         public string GetAllProducts()
         {
-            //Modicafores de acceso 
-            //Tipos de retorno de un metodo,void,string
-            //Parametros
-            //Swagger que es ? en un api como se relaciona 
-
             return "Todos los productos";
         }
 
-        //2. Metodo GetProduct
-
-        [HttpGet("GetProduct")]
+        [HttpGet("GetProductById")]
         public IActionResult GetProduct(int id)
         {
             var product = new { Id = id, Name = "Product " + id };
             return Ok(product);
         }
 
-        //3. Crear Una Lista de Productos 
+        [HttpPost("AddProduct")]
+        public IActionResult AddProduct()
+        {
+            return Ok();
+        }
 
-        [HttpGet("GetProductsList")]
+        [HttpPut("UpdateProduct")]
+        public IActionResult Update()
+        {
+            return Ok();
+        }
+
+        [HttpDelete("DeleteProduct")]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+
+   
         public List<string> GetProductsList()
         {
             return new List<string>
